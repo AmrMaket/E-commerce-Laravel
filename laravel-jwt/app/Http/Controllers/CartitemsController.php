b0f2d61378b55626342f4b76e9643bbccd80d8b9
Amr Maket <amrmaket89@gmail.com>
2023-07-31 20:03:10 +0300

    added cartitemsController

--- laravel-jwt/app/Http/Controllers/CartitemsController.php
+++ laravel-jwt/app/Http/Controllers/CartitemsController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cart;
 
-class CartitemsController extends Controller
+
+class CartController extends Controller
 {
-    //
+    function addToCart (Request $request) 
+    {
+        $cart = new Cart;
+        $cart -> user_id = $request -> user_id;
+        $cart -> product_id = $request -> product_id;
+        $cart -> save();
+
+        return json_encode(["cart_products" => $cart]);
+    }
+
+    function viewCart () 
+    {
+        
+    }
 }
